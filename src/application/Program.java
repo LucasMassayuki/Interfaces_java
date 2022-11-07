09056a09d783d5a0bf6baafd28a26b87e2ccf14d
Lucas <lucasofficialcruz95@gmail.com>
2022-11-07 18:45:47 -0300

    Herdar vs cumprir contrato

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,46 +1,22 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Scanner;
 
-import entities.Contract;
-import entities.Installment;
-import services.ContractService;
-import services.PaypalService;
+
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		//definir o formato de Date
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 		
-		System.out.println("Entre os dados do contrato:");
-		System.out.print("Numero:");
-		int number = sc.nextInt();
-		System.out.print("Data (dd/MM/yyyy): ");
-		LocalDate date = LocalDate.parse(sc.next(), fmt);
-		System.out.print("Valor do contrato: ");
-		double totalValue = sc.nextDouble();
-		
-		Contract obj = new Contract(number, date, totalValue);
-		
-		System.out.print("Entre com o numero de parcelas: ");
-		int n = sc.nextInt();
-		
-		ContractService	contractService = new ContractService(new PaypalService());
-		
-		contractService.processContract(obj, n);
-		
-		System.out.println("Parcelas:");
-		for(Installment installment : obj.getInstalments()) {
-			System.out.println(installment);
-		}
-		
-		sc.close();
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle color: " + String.format("%.3f", s1.area()));
 	}
 
 }
--- src/services/OnlinePaymentService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public interface OnlinePaymentService {
-	double paymentfee(double amount);
-	double interest(double amount, int months);
-	
-}
